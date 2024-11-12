7af80783717081a6901ec436151e084691f00674
Aaron Amoso <amosoaaron@gmail.com>
2024-11-12 01:14:44 -0700

    // continue with the logic for only allow two decimals

--- javaScript_files/splitEqually.js
+++ javaScript_files/splitEqually.js
@@ -1,5 +1,4 @@
-let billName;
-let totalBill;
+let numbersOnly = /^\d*\.?\d+$/;
 let tipPercentage;
 let numOfPeople;
 
@@ -7,20 +6,26 @@ let numOfPeople;
 
 document.getElementById("mySubmit").onclick = function () { // this is the form or how to access
     // billName LOGIC
-    billName = document.getElementById("billName").value; // this gets the value of the input for billName
-    console.log(billName); // for testing
+    // let billName = document.getElementById("billName").value; // this gets the value of the input for billName
+    // console.log(billName); // for testing
 
-    // Uses .test(string) method bc it finds a match in a string, if it does, it returns true, otherwise it returns false
-    // regEx
-    let isStringOnly = /^[A-Za-z']+$/;
+    // // Uses .test(string) method bc it finds a match in a string, if it does, it returns true, otherwise it returns false
+    // // regEx
+    // let isStringOnly = /^[A-Za-z']+$/;
 
-    if (isStringOnly.test(billName)) {
-        console.log("success")
-    } else {
-        console.log("Enter a valid name!")
-    } 
+    // if (isStringOnly.test(billName)) {
+    //     console.log("success")
+    // } else {
+    //     console.log("Enter a valid name!")
+    // } 
 
     // totalBill LOGIC
-    totalBill = document.getElementById("totalBill").value;
-    console.log(totalBill);
+    let totalBill = document.getElementById("totalBill").value;
+    if (numbersOnly.test(totalBill)) {
+        console.log("success")
+    } else {
+        console.log("enter a num") 
+        // continue with the logic for only allow two decimals
+    }
+
 }
