393716e3365fbd7d80d6fe23155c7f3455a9ce98
aaronamoso <amosoaaron@gmail.com>
2025-04-27 01:02:49 -0600

    edited server.js -- changes port for the backend

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,6 @@ const mongoose = require('mongoose');  // This connects to the database (like op
 const bodyParser = require('body-parser');  // Parses incoming JSON data from the frontend
 const cors = require('cors');  // Lets the frontend and backend talk even if they're in different places
 const dotenv = require('dotenv');  // Manages secret keys and passwords
-const path = require('path');  // Helps find files and folders
 const app = express();;
 
 // ROUTES (Tasks the server handles)
@@ -16,22 +15,14 @@ const pageRoutes = require('./routes/pageRoutes');  // Handles extra pages
 dotenv.config();  // Load environment variables from .env file
 
 // Middleware (Tools for Express)
+app.use(cors());
 app.use(express.json());  // Lets the server understand JSON data
 app.use(express.urlencoded({ extended: true }));  // Lets the server understand form data
-
-
-// Catch-all route to serve index.html
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../frontend/index.html'));  // Always show the frontend page
-  });
   
 // Routes
 app.use('/', pageRoutes);  // Serve registration and login pages
 app.use('/', homepageRoutes);  // Show the homepage
 app.use('/auth', authRoutes);  // Handle registration and login requests
-
-// Routes for React frontend
-
 
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI)  // Connect to the database
@@ -39,6 +30,7 @@ mongoose.connect(process.env.MONGO_URI)  // Connect to the database
     .catch((err) => console.error('Error connecting to MongoDB:', err));  // Error message
 
 // Start the server and listen on port 3000
-app.listen(3000, () => {
-    console.log(`Server running on http://localhost:3000`);  // Server confirmation message
+app.listen(5000, () => {
+    console.log(`Server running on http://localhost:5000`);  // Server confirmation message
 });
+
